7fb79564d5bb4ceee8ca850f10e11408683b7691
Xiaoyan Wang <165269103+XiaoyanWang11@users.noreply.github.com>
2024-04-04 18:05:08 +0800

    Update FAD_FB.IB.BS.R

--- FAD_FB.IB.BS.R
+++ FAD_FB.IB.BS.R
@@ -11,7 +11,6 @@ opt <- list(input_FB = "/../FAD_FB/",
             input_BS = "/../FAD_BS/",
             opd = "/../FAD_FB.MB.BS/")
 ### import rds file which has been removed doublets by DoubletFinder function
-
 CellBender_FB <- readRDS(paste0(opt$input_FB, "FAD_FB_DoubletRD.rds"))
 CellBender_MB <- readRDS(paste0(opt$input_MB, "FAD_MB_DoubletRD.rds"))
 CellBender_BS <- readRDS(paste0(opt$input_BS, "FAD_BS_DoubletRD.rds"))
@@ -36,8 +35,23 @@ Obj_Integrated <- RunUMAP(Obj_Integrated, dims = 1:nPCs, reduction = "pca", verb
 Obj_Integrated <- FindNeighbors(Obj_Integrated, reduction = "pca", dims = 1:nPCs)
 Obj_Integrated <- FindClusters(Obj_Integrated, resolution = 0.3)
 
-# add region group
+# add diet group
+group_diet <- data.frame(orig.ident = c('Mu93FB_1', 'Mu93FB_2', 'Mu93FB_3', 'Mu93MB_1', 'Mu93MB_2', 'Mu93MB_3',
+                                        'Mu93BS_1', 'Mu93BS_2', 'Mu93BS_3',
+                                        'MuLIFB_1' , 'MuLIFB_2' , 'MuLIFB_3', 'MuLIMB_1' , 'MuLIMB_2' , 'MuLIMB_3',
+                                        'MuLIBS_1' , 'MuLIBS_2' , 'MuLIBS_3'),
+                         group = c('Ctrl_AD','Ctrl_AD','Ctrl_AD', 'Ctrl_AD','Ctrl_AD','Ctrl_AD',
+                                   'Ctrl_AD','Ctrl_AD','Ctrl_AD', 
+                                   'Inulin_AD','Inulin_AD','Inulin_AD', 'Inulin_AD','Inulin_AD','Inulin_AD',
+                                   'Inulin_AD','Inulin_AD','Inulin_AD'))
 
+orig <- data.frame(orig.ident = Obj_Integrated@meta.data$orig.ident)
+new1 <- merge(orig, group_diet, by = "orig.ident")
+
+Obj_Integrated$diet <- new1$group
+Obj_Integrated[['diet']] <- new1$group[match(Obj_Integrated$orig.ident, new1$orig.ident)]
+
+# add region group
 group_region <- data.frame(orig.ident = c('Mu93FB_1', 'Mu93FB_2', 'Mu93FB_3', 'MuLIFB_1' , 'MuLIFB_2' , 'MuLIFB_3',
                                           'Mu93MB_1', 'Mu93MB_2', 'Mu93MB_3', 'MuLIMB_1' , 'MuLIMB_2' , 'MuLIMB_3',
                                           'Mu93BS_1', 'Mu93BS_2', 'Mu93BS_3', 'MuLIBS_1' , 'MuLIBS_2' , 'MuLIBS_3'),
@@ -47,10 +61,8 @@ group_region <- data.frame(orig.ident = c('Mu93FB_1', 'Mu93FB_2', 'Mu93FB_3', 'M
 
 orig <- data.frame(orig.ident = Obj_Integrated@meta.data$orig.ident)
 new1 <- merge(orig, group_region, by = "orig.ident")
-
 Obj_Integrated$region <- new1$group
 Obj_Integrated[['region']] <- new1$group[match(Obj_Integrated$orig.ident, new1$orig.ident)]
-
 saveRDS(Obj_Integrated, file = paste0(opt$opd, "FAD_FB.MB.BS_res0.3_", Sys.Date(), ".rds"))
 
 combine_colors <- c("Neuron" = "palevioletred2", "Astrocyte" = "olivedrab2", "Microglia" = "mediumpurple1",
@@ -84,7 +96,7 @@ DimPlot(Obj_Integrated, reduction = "umap", label = T, repel = T, label.color = 
         label.size = 6, group.by = "ident", split.by = "region",  raster=FALSE)
 ggsave(paste0(opt$opd, gg_title, "_DimPlot_",Sys.Date(),".png"))
 
-### subset each cell type and re-clustering
+## subset each cell type and re-clustering
 # subset Astrocyte
 gg_title <- "Astrocyte"
 Obj_Astrocyte <- find_cluster(Obj_Integrated, idents = "Astrocyte", res = 0.3, gg_title = gg_title)
@@ -101,10 +113,8 @@ Obj_Microglia2 <- find_cluster1(Obj_Microglia, gg_title = gg_title)
 # find markers for each cluster
 AllMarkers <- FindAllMarkers(Obj, only.pos = TRUE, min.pct = 0.25,  # upregulation
                              logfc.threshold = 0.3, assay = "RNA") %>% filter(p_val_adj < 0.05)
-
 # GO-BP pathway for each cluster
 DEG_GO <- function(Seurat_DEG_file, nGO =10, gg_title){
-  
   DEG_list <- split(Seurat_DEG_file$gene, f = Seurat_DEG_file$cluster)
   
   # convert gene symbol to Entrez id
@@ -134,6 +144,7 @@ DEG_GO <- function(Seurat_DEG_file, nGO =10, gg_title){
     theme(axis.title.y.left = element_blank())
   ggsave(paste0(opt$opd, gg_title,"_TopGO-BP_", Sys.Date(), ".png"), width = 8, height = 10)
 }     
+gg_title <- "Astrocyte"; Obj <- Obj_A; DEG <- DEG_GO(Astrocyte_AllMarkers, gg_title = gg_title)
 
 
 
